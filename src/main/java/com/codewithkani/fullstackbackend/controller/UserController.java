f6d87521e4cee3b1a3e809498799da955c2d24c3
kgureye <kanigureye@outlook.com>
2023-07-04 15:12:03 +0200

    "Added PostMapping to send data"

---
+++ src/main/java/com/codewithkani/fullstackbackend/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.codewithkani.fullstackbackend.controller;
+import com.codewithkani.fullstackbackend.repository.UserRepository;
+import com.codewithkani.fullstackbackend.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+}
